02ca1eccd7528f45d7071f891d13c2b0c7c2fea7
iisuslik9 <150892541+iisuslik9@users.noreply.github.com>
2024-11-13 15:34:15 +0700

    Add files via upload
    агрегация и композиция классов
    mixin

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,149 +1,134 @@
-﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-#include <iostream>
-#include<string>
-#include<fstream>
-#include<sstream>
-#include<Windows.h>
-
+﻿#include <iostream>
+#include <string>
 using namespace std;
-
-struct student
-{
-    string first_name;
-    string last_name;
-    short marks;
-};
-
-void PrintStudent(student st)
-{
-    cout << "" + st.last_name +
-        st.first_name + "";
-    for (int i = 0; i < 3; i+++)
-    {
-        cout << st.marks[i] << "";
+//класс mixins
+class Mixin {
+public:
+    void LogAction(const string& action) {
+        cout << action << endl;
     }
-    cout << endl;
+};
+//класс оружие. оружие может существовать отдельно от персонажа
+class Weapon {
+    //стоит сделать поля приватными и дописать гетеры и сеттеры
+public:
+    string name; //название оружия
+    int damage; //урон
+
+    Weapon(const string& n, int d) : name(n), damage(d) {};
+    
+    void Print() const {
+        cout << "оружие: " << name << ", урон: " <<damage <<endl;
+    }
+};
+//класс заклинание. Заклинание не может существовать без персонажа
+class Spell {
+public:
+    int fireball_damage;//урон 
+    int healing_amount;//сколько здоровья восстанавливает
+
+    Spell(int f, int h) : fireball_damage(f), healing_amount(h) {};
+    void Print() const {
+        cout << "урон от огненого шара: " << fireball_damage << ", исцеление на " << healing_amount << " едениц"<< endl;
+    }
+};
+//базовый класс для персонажа
+class Character {
+protected:
+    string name;
+    int health;
+public:
+    Character(const string& n, int h) : name(n), health(h) {};
+    //виртуальный метод, который будем переопределять в потомках
+    virtual void Print() const {
+        cout << "Персонаж: " << name << ", здоровье:" << health << endl;
+    }
+    //чистый виртуальный метод, обязательно должен быть переопределен в классах-наследниках
+    virtual void Attack(Character& target) = 0;
+    //проверка жив ли персонаж
+    bool IsAlive() const {
+        return health > 0;
+    }
+    //метод получения урона
+    void TakeDamage(int damage) {
+        health -= damage;
+        if (health < 0) health = 0;
+        if (health == 0) {
+            cout << name << " повержен(а)" << endl;
+        }
+    }
+    friend void Fight(Character& pers1, Character& pers2);
+};
+//производный класс воин
+class Warrior : public Character, public Mixin {
+private:
+    //создание оружия по указателю
+    Weapon* weapon; //агрегация классов. Оружие может существовать независимо от воина
+public:
+    Warrior(const string& n, int h, Weapon* w) : Character(n, h), weapon(w) {};
+    //переопределяем метод
+    void Print() const override {
+        Character::Print(); //так как наследуемся от character
+        //weapon->Print();//вызываем от указателя
+    }
+    //переопределяем чистый виртуальный метод базового класса
+    void Attack(Character& target) override {
+        LogAction(name + " атакует, используя " + weapon->name + "!");
+        target.TakeDamage(weapon->damage);
+    }
+};
+//производный класс маг
+class Mage : public Character, public Mixin {
+private:
+    //создание объекта
+    Spell spell; //композиция классов. Заклинание не может существовать независимо от мага
+public:
+    Mage(const string& n, int h, Spell s) : Character(n, h), spell(s) {};
+    //переопределяем метод
+    void Print() const override {
+        Character::Print(); //так как наследуемся от character
+        //spell.Print();//вызываем от объекта
+    }
+    //восстановление здоровья
+    void RestoreHealth() {
+        health += spell.healing_amount;
+        LogAction(name + " восстанавливает здоровье на " + to_string(spell.healing_amount) + " едениц" + "!");
+    }
+    //переопределяем чистый виртуальный метод базового класса
+    void Attack(Character& target) override {
+        LogAction(name + " атакует, используя огненный шар" + "!");
+        target.TakeDamage(spell.fireball_damage);
+        RestoreHealth();
+        
+    }
+};
+void Fight(Character& pers1, Character& pers2) {
+    //создание строки из большого количества символов
+    string line(40, '-');
+    cout << line << endl;
+    cout << "Бой между "<<pers1.name << " и " << pers2.name<< "."<<endl;
+    while (pers1.IsAlive() && pers2.IsAlive()) {
+        pers1.Attack(pers2);
+        
+        if (!pers2.IsAlive()) break;
+        pers2.Attack(pers1);
+        pers1.Print();
+        pers2.Print();
+    }
+    cout << "бой завершен" << endl;
+    cout << line << endl;
 }
-
-bool ReadFile(const string file_name, student*& array, int& n);
-
-//struct point
-//{
-//    int x;
-//    int y;
-//}C, D = {8,1},F;
-//
-//struct
-//{
-//    int x;
-//    int y;
-//}K, L = { 15,18 };
-//
-//
-//
-//void PrintPoint(string name, point T)
-//{
-//    cout << "" + name + "\n";
-//    cout << "координата x=" << T.x << "\n";
-//    cout << "координата y=" << T.y << "\n";
-//}
-
 int main()
 {
     setlocale(LC_ALL, "rus");
-    /*    point A;
-        point B;
+    Weapon* sword = new Weapon("меч", 15);
+    Spell spell(10, 2);
+    Character* pers1 = new Warrior("Гера", 40, sword);
+    Character* pers2 = new Mage("Оптимус прайм", 55, spell);
+    Fight(*pers1, *pers2);
 
-        A.x = 15;
-        A.y = 30;
-
-        PrintPoint("A", A);
-        PrintPoint("B", B);
-        C.x = 75;
-        C.y = 10;
-
-        PrintPoint("C",C);
-        PrintPoint("D", D);
-        PrintPoint("F", F);
-        PrintPoint("J", J);
-
-        cout << K.x << ""<<L.y*/
-    student Anton, Artem = {Артем};
-
-
-
-        Anton.first_name = "Артем";
-        Anton.last_name = "Бродяк";
-        Anton.marks[0] = 2;
-        Anton.marks[1] = 3;
-        Anton.marks[2] = 4;
-
-        student* array;
-        string file_name = "in.txt";
-        int n;
-
-        if (!ReadFile(file_name, array, n))
-        {
-            return 1;
-        }
-        for (int i=0;i<n;i++)
-
-
-
-
-        return 0;
-
+    return 0;
 }
 
-bool ReadFile(const string file_name, student*& array, int& n)
-{
-    ifstream fin;
-    fin.open(file_name);
-    if (!fin.is_open())
-    {
-        cout << "" + file_name + "!\n";
-        return 0;
-    }
-    stringstream obj;
-    string str;
-    int count = 0;
-    while (getline(fin, str))
-    {
-        obj << str + "\n";
-        count++;
-    }
-    fin.close();
-    n = count;
-    array = new student[n];
-    for (int i = 0; i < n; i+++)
-    {
-        getline(obj, str);
-        int index = str.find(' ');
-        array[i].last_name =
-            str.substr(0, index);
-        str = str.substr(index + 1);
 
-        int index = str.find(' ');
-        array[i].first_name =
-            str.substr(0, index);
-        str = str.substr(index + 1);
-
-        int index = str.find(' ');
-        array[i].marks[j] =
-            stoi(str.substr(0, index));
-        str = str.substr(index + 1);
-    }
-
-}
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+//контрольная полиформизм наследование. в четверг консультация пятой парой
